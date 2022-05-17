079e4d04230d9da1559b50754fea64453e6a87ad
zhusupmamatzhanov <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-05-17 20:20:28 +0600

    Initial commit

---
+++ src/com/company/Triangle.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Triangle extends Shape{
+    private int c;
+    public Triangle(int a, int b,int c) {
+        super(a, b);
+        this.c= c;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    @Override
+    public int getPerimeter() {
+        System.out.print("Triangle's perimeter= "+ getA()+getB()+getC());
+        return 0;
+    }
+}
